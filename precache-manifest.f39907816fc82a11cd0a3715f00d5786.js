88db017cf194eb633675c3fbadb3304c9a192505
pabirtz <pabirtz@gmail.com>
2020-07-06 09:17:59 -0400

    Updates

---
+++ precache-manifest.f39907816fc82a11cd0a3715f00d5786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3891db27f0aa35adf5093b6bcb978e0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a6a64140b235293140c7",
+    "url": "/monsters-rolodex/static/css/main.6c29df0b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a64140b235293140c7",
+    "url": "/monsters-rolodex/static/js/main.0f85a926.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
